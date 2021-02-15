a3ca5a2dbf8b68aeb2b5994be5b3b5d85939fae6
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-15 23:27:57 +0200

    11

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+const link = React.createElement('a', { href: 'reactjs.org', target: '_blank' }, "ссылка");
 
+const jsxLink = <a href='reactjs.org' target='_blank'>ссылка</a>;
+console.log(link);
+console.log(jsxLink);
+// ReactDOM.render(link, document.getElementById('root'));
