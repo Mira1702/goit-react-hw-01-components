2d46c5a7af4c1c73918b971286170b57f65d5e0a
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-17 23:23:50 +0200

    создает разметку FriendList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Profile from './components/Profile';
 import Statistics from './components/Statistics';
+import FriendList from './components/FriendList';
 import user from './DataJSON/user.json';
 import statisticalData from './DataJSON/statistical-data.json';
+import friends from './DataJSON/friends.json';
 
 const App = () => {
     return (
@@ -16,6 +18,7 @@ const App = () => {
             />
             <Statistics title="Upload stats" stats={statisticalData} />;
             {/* <Statistics stats={statisticalData} />; */}
+            <FriendList friends={friends} />
         </div>
     )
 }
