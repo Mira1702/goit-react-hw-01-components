80d883e65e70be47df09d960c52e61f3255e61cd
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-17 22:43:19 +0200

    создает разметку Statistics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Profile from './components/Profile';
-import user from './DataJSON/user.json'
+import Statistics from './components/Statistics';
+import user from './DataJSON/user.json';
+import statisticalData from './DataJSON/statistical-data.json';
 
 const App = () => {
     return (
@@ -12,6 +14,8 @@ const App = () => {
               avatar={user.avatar}
               stats={user.stats}
             />
+            <Statistics title="Upload stats" stats={statisticalData} />;
+            {/* <Statistics stats={statisticalData} />; */}
         </div>
     )
 }
