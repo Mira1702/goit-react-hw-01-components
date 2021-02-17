86a1a30008a1b0855d4aa4b5f8f00fd6aec54382
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-17 22:34:58 +0200

    создает разметку Profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-const link = React.createElement('a', { href: 'reactjs.org', target: '_blank' }, "ссылка");
-
-const jsxLink = <a href='reactjs.org' target='_blank'>ссылка</a>;
-console.log(link);
-console.log(jsxLink);
-// ReactDOM.render(link, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
