0d0a5afe77692735f4c3747e1cd171254ac1812e
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-19 00:17:12 +0200

    11

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import FriendList from './components/FriendList/FriendList.js';
 import TransactionHistory from './components/Transaction/TransactionHistory.js';
 
 import user from './DataJSON/user.json';
-import statisticalData from './DataJSON/statistical-data.json';
+import statisticalData from './DataJSON/statisticalData.json';
 import friends from './DataJSON/friends.json';
 import transactions from './DataJSON/transactions.json';
 
@@ -20,8 +20,8 @@ const App = () => {
               avatar={user.avatar}
               stats={user.stats}
             />
-            <Statistics title="Upload stats" stats={statisticalData} />
-            {/* <Statistics stats={statisticalData} />; */}
+            {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
+            <Statistics stats={statisticalData} />
             <FriendList friends={friends} />
             <TransactionHistory items={transactions} />
         </div>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
-const Statistics = () => {
+const StatisticListItem = ({ id, label, percentage }) => {
+  return (
+    <li key={id} className="item">
+      <span className="label">{label}</span>
+      <span className="percentage">{percentage}</span>
+    </li>
+  )
+}
+
+const StatisticList = ({ items }) => {
+  if (items.length == 0) return null
+  return <ul className="stat-list">{items.map(StatisticListItem)}</ul>
+}
+
+
+const Statistics = ({ items }) => {
     return (
         <section className="statistics">
          <h2 className="title">Upload stats</h2>
 
-          <ul className="stat-list">
-            <li className="item">
-              <span className="label">.docx</span>
-              <span className="percentage">4%</span>
-            </li>
-            <li className="item">
-              <span className="label">.mp3</span>
-              <span className="percentage">14%</span>
-            </li>
-            <li className="item">
-              <span className="label">.pdf</span>
-              <span className="percentage">41%</span>
-            </li>
-            <li className="item">
-              <span className="label">.mp4</span>
-              <span className="percentage">12%</span>
-            </li>
-          </ul>
+        <StatisticList items={items} />
         </section>
     )
 }
