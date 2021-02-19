4c9e4d6186a6654b7a7a11cbd070a40933c2d408
Myroslava <67161084+Mira1702@users.noreply.github.com>
2021-02-19 22:43:33 +0200

    Statistics done

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
               avatar={user.avatar}
               stats={user.stats}
             />
-            {/* <Statistics title="Upload stats" stats={statisticalData} /> */}
+            <Statistics title="Upload stats" stats={statisticalData} />
             <Statistics stats={statisticalData} />
             <FriendList friends={friends} />
             <TransactionHistory items={transactions} />
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import s from './Statistics.module.scss';
 
 const StatisticListItem = ({ id, label, percentage }) => {
   return (
-    <li key={id} className="item">
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}</span>
+    <li key={id} className={s.item} style={{ backgroundColor: randomColor() }}>
+      <span className={s.label}>{label}</span>
+      <span className={s.percentage}>{percentage}%</span>
     </li>
   )
 }
 
-const StatisticList = ({ items }) => {
-  if (items.length == 0) return null
-  return <ul className="stat-list">{items.map(StatisticListItem)}</ul>
+const StatisticList = ({ stats }) => {
+  if (stats.length == 0) return null
+  return <ul className={s.statList}>{stats.map(StatisticListItem)}</ul>
 }
 
 
-const Statistics = ({ items }) => {
+const Statistics = ({ stats }) => {
     return (
-        <section className="statistics">
-         <h2 className="title">Upload stats</h2>
+        <section className={s.statistics}>
+         <h2 className={s.title}>Upload stats</h2>
 
-        <StatisticList items={items} />
+        <StatisticList stats={stats} />
         </section>
     )
+}
+
+function randomColor()
+{
+     return '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
 }
 
 export default Statistics;
